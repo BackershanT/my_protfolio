8ce7610ec6de09651528f2558fe3c1e2caa86016
Backershan T <98761199+BackershanT@users.noreply.github.com>
2025-07-29 20:03:19 +0530

    updated mern

--- lib/src/constants/skill_items.dart
+++ lib/src/constants/skill_items.dart
@@ -11,27 +11,33 @@ const List<Map> platformItems = [
 ];
 
 List<Map> skillItems = [
+  //flutter
   {"img": "assets/skills/flutter/flutter.png", "title": "Flutter "},
   {"img": "assets/skills/flutter/dart.png", "title": "Dart "},
   {"img": "assets/skills/flutter/bloc.png", "title": "Bloc "},
-  {"img": "assets/skills/flutter/flame.png", "title": "Flame "},
-  {"img": "assets/skills/flutter/supabase.png", "title": "Supabase "},
+  // {"img": "assets/skills/flutter/flame.png", "title": "Flame "},
 
- {"img": "assets/skills/react/react.png", "title": "React "},
+//react
+  {"img": "assets/skills/react/react.png", "title": "React "},
   {"img": "assets/skills/react/redux.png", "title": "Redux "},
 
+//web
   {"img": "assets/skills/html/html.png", "title": "HTML 5"},
   {"img": "assets/skills/html/css.png", "title": "css "},
+  {"img": "assets/skills/python/python.png", "title": "Python "},
+  {"img": "assets/skills/python/django.png", "title": "Django "},
 
- {"img": "assets/skills/python/python.png", "title": "Python "},
- {"img": "assets/skills/python/django.png", "title": "Django "},
+  //mern
 
+  {"img": "assets/skills/mern/mongo.webp", "title": "MongoDB "},
+  {"img": "assets/skills/mern/express.png", "title": "Express.js "},
+  {"img": "assets/skills/react/react.png", "title": "React "},
+  {"img": "assets/skills/mern/nodeee.webp", "title": "Node.js "},
 
-
+  //tools
 
   {"img": "assets/skills/design/figma.png", "title": "Figma "},
   {"img": "assets/skills/design/adobexd.png", "title": "Adobe XD "},
-
- {"img": "assets/skills/flutter/firebase.png", "title": "Firebase "},
-
+  // {"img": "assets/skills/design/canva.png", "title": "Canva "},
+  {"img": "assets/skills/flutter/firebase.png", "title": "Firebase "},
 ];
